21d5b75705118ce82ae9ae652129f332ae869a6a
Jeandre Awood <jeandreross@gmail.com>
2021-07-28 16:27:58 +0200

    update

--- app.py
+++ app.py
@@ -76,6 +76,11 @@ app.config['SECRET_KEY'] = 'super-secret'
 jwt = JWT(app, authenticate, identity)
 
 
+@app.route('/')
+def home():
+    return "Welcome, This is the Home Page"
+
+
 @app.route('/protected')
 @jwt_required()
 def protected():
